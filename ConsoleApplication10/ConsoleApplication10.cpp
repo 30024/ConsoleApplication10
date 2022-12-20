0f258c4777cbc4e44f9fcc0c6995545a930da156
admin-gr00 <admin-gr00@AM-T-07404>
2022-12-20 10:55:38 +0100

    dodanie for

--- ConsoleApplication10/ConsoleApplication10.cpp
+++ ConsoleApplication10/ConsoleApplication10.cpp
@@ -19,5 +19,13 @@ int odkrycie;
 };
     pole plansza[8][8];
 
+    generuj_plansze();
+    {
+        for (int=0;x<8;x++)
+            for (int=0; y>8; y++)
+            {
+                plansza[x][y].wartsoc = 0;
+
+
 
 
